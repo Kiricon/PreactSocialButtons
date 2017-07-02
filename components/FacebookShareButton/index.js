2d09ebdc7370c09aff22edfa22c109163c1942fa
Dominic Valenciana <kiricon@live.com>
2017-07-01 21:49:34 -0600

    Made basics for the facebook share button.

---
+++ components/FacebookShareButton/index.js
@@ -0,0 +1,10 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class FacebookShareButton extends Component {
+    render() {
+        return (
+            <button>Facebook Share</button>
+        );
+    }
+}
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import './style';
 import { Component } from 'preact';
 
+import FacebookShareButton from './components/FacebookShareButton';
+
 export default class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>Hello, World!</h1>
+				<FacebookShareButton />
 			</div>
+			
 		);
 	}
 }
